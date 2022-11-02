9d753377ccd26e68bccbdf0a1b4d1101d19e52b1
Angelica <mmaderacovo@gmail.com>
2022-11-02 17:05:10 -0500

    Archivos iniciales

---
+++ src/Isis.java
@@ -0,0 +1,9 @@
+public class Isis {
+    public static String nombreCompleto = "Angelica Madera";
+    public static String edad = "25 años";
+
+  public static void main (String [] args) {
+      System.out.println(nombreCompleto+" "+edad);
+  }
+
+}
