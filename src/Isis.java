2135b1ff63bf0ea156b68f4beeaae42da7bf68cd
Angelica <mmaderacovo@gmail.com>
2022-11-11 17:21:24 -0500

    tipode datos e IF

--- src/Isis.java
+++ src/Isis.java
@@ -1,9 +1,32 @@
 public class Isis {
-    public static String nombreCompleto = "Angelica Madera";
-    public static String edad = "25 años";
+    public static void main(String[] args) {
+        System.out.println("Angelica");
+        String name = "Juan";
+        System.out.println(name);
+        int numerito = 15;
+        System.out.println(numerito);
+        boolean falsito = false;
+        System.out.println(falsito);
+        char verdadero = 'v';
+        System.out.println(verdadero + name);
+        if (3 > 6) {
+            System.out.println("Es verdadero");
+        } else {
+            System.out.println("Es falso");
+        }
+        if (falsito) {
+            System.out.println(false);
+        } else {
+            System.out.println(verdadero);
+        }
+        int entero = 5;
+        int enterito = 5;
+        if (entero == enterito) {
+            System.out.println("igualito");
+        } else {
+            System.out.println("Que te pasa llave");
+        }
+    }
 
-  public static void main (String [] args) {
-      System.out.println(nombreCompleto+" "+edad);
-  }
 
 }
